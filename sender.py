413ddef739b9249ffd129408bfb9b65467cf28c6
Stephan Wiesenhütter <swiesenhuetter@amberg.ch>
2020-11-05 22:42:47 +0100

    linux and windows signals

--- sender.py
+++ sender.py
@@ -10,8 +10,8 @@ until stop_event occurs
 """
 
 if __name__ == "__main__":
-    """ process start time in ms"""
     t0 = int(1000.0 * time.time())
+    """ process start time in ms"""
 
     if len(sys.argv) > 1:
         interval = float(sys.argv[-1])
@@ -24,7 +24,11 @@ if __name__ == "__main__":
     def shut_down(signal_received, frame):
         stop_evt.set()
     signal.signal(signal.SIGINT, shut_down)
-    signal.signal(signal.SIGBREAK, shut_down)
+    if os.name == 'nt':
+        signal.signal(signal.SIGBREAK, shut_down)
+    else :
+        signal.signal(signal.SIGTERM, shut_down)
+
 
     while True:
         t_now = int(1000.0*time.time()) - t0
