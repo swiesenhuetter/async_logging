ffa8c93a6124351dfb520ffa324d9eafe2e3c6d5
Stephan Wiesenhütter <swiesenhuetter@amberg.ch>
2020-11-08 23:20:17 +0100

    gnu c++ (linux compatible)

--- cpp_primes/primes.h
+++ cpp_primes/primes.h
@@ -4,9 +4,20 @@
 
 
 // C - interface
-#define PRIMES_API extern "C" __declspec(dllexport)
+#if defined (_MSC_VER)
+    // MS Visual C++
+    #define EXPORT __declspec(dllexport)
+#elif defined (__GNUC__)
+    //  GCC
+    #define EXPORT __attribute__((visibility("default")))
+#else
+    //  do nothing and hope for the best?
+    #define EXPORT
+    #pragma warning Unknown dynamic link import/export semantics.
+#endif
+
 
 namespace primes
 {
-    PRIMES_API bool is_prime(uint64_t n);
+    EXPORT bool is_prime(uint64_t n);
 }
